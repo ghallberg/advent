05b2e0cfef8bde6d8a34a6ae38ccb3f189c373cb
Gustaf Hallberg <ghallberg@gmail.com>
2016-12-08 09:09:56 +0100

    Start of day 4

--- src/advent/day_4.clj
+++ src/advent/day_4.clj
@@ -1,8 +1,30 @@
 (ns advent.day-4)
 
-(use '[clojure.string :only (split trim)])
+(use '[clojure.string :only (split replace)])
+
+(defn split-descriptor
+  [descriptor]
+  (rest (re-find #"(\D+)-(\d+)\[(.*)\]" descriptor)))
+
+(defn parse-parts
+  [descriptor]
+  (let [[crypt-name sector-id checksum] (split-descriptor descriptor)]
+    {:name (replace crypt-name "-" ""),
+     :sector sector-id,
+     :checksum checksum}))
+
+(defn parse-rooms
+  [input]
+  (map parse-parts (split input #"\n")))
+(defn clean-vec
+  [input]
+  (map first input))
+
+(defn calculate-checksum
+  [input]
+  (clean-vec (sort-by key > (group-by second (frequencies input)))))
 
 
 (defn solve
   [input]
-  input)
+  (parse-rooms input))
