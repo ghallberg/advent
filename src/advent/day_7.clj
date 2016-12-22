07fb01e408149d04e1d71166d64a296ce4716ff9
Gustaf Hallberg <ghallberg@gmail.com>
2016-12-22 08:58:23 +0100

    Day 7 start

--- src/advent/day_7.clj
+++ src/advent/day_7.clj
@@ -1,7 +1,19 @@
 (ns advent.day-6
   (:require [clojure.string :as s]))
 
+(defn parse-input
+  [input]
+  input)
+
+(defn abba?
+  [string]
+  string)
+
+(defn tls?
+  [outside hypernet]
+  (and (any? abba? outside) (not (any? abba? hypernets))))
 
 (defn solve
   [input]
-  input)
+  (let [address-tuples (parse-input input)]
+    (count (filter tls? address-tuples))))
